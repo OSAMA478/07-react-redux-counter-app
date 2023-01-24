081ae059a91952d36947181f908e170338b49601
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-24 22:31:58 +0530

    completed with redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,21 +3,29 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
 	const counter = useSelector((state) => state.counter);
+	const show = useSelector((state) => state.showCounter);
 	const dispatch = useDispatch();
-	const toggleCounterHandler = () => {};
 
 	const incrementHandler = () => {
 		dispatch({ type: "increment" });
 	};
+
+	const increaseHandler = () => {
+		dispatch({ type: "increase", amount: 5 });
+	};
 	const decrementHandler = () => {
 		dispatch({ type: "decrement" });
+	};
+	const toggleCounterHandler = () => {
+		dispatch({ type: "toggle" });
 	};
 	return (
 		<main className={classes.counter}>
 			<h1>Redux Counter</h1>
-			<div className={classes.value}>{counter}</div>
+			{show && <div className={classes.value}>{counter}</div>}
 			<div className="flex justify-center gap-2 mb-2">
 				<button onClick={decrementHandler}>Decrement</button>
+				<button onClick={increaseHandler}>Increase by 5</button>
 				<button onClick={incrementHandler}>Increment</button>
 			</div>
 			<button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
 const redux = require("redux");
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
 	switch (action.type) {
 		case "increment":
 			return {
+				...state,
 				counter: state.counter + 1,
 			};
 			break;
 		case "decrement":
 			return {
+				...state,
 				counter: state.counter - 1,
+			};
+			break;
+		case "increase":
+			return {
+				...state,
+				counter: state.counter + action.amount,
+			};
+			break;
+		case "toggle":
+			return {
+				...state,
+				showCounter: !state.showCounter,
 			};
 			break;
 		default:
